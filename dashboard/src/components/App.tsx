cff7b09af20f5580662738445e22dd095605a5a9
amaralBruno27866 <bamaral2.git2023@gmail.com>
2023-08-31 15:21:01 -0400

    Changing the App.tsx and applying style

---
+++ dashboard/src/components/App.tsx
@@ -0,0 +1,11 @@
+import style from '../styles/App.module.css'
+import '../styles/global.css'
+
+export function App() {
+
+  return (
+    <div className={style.app}>
+      <h1>Test</h1>
+    </div>
+  )
+}
